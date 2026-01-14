5cf0503c6e43c510903e21bedf6ee61404627f15
강승우 <phonil@naver.com>
2026-01-14 22:39:34 +0900

    init

---
+++ src/main/java/com/example/only4_kafka/Only4KafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.only4_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Only4KafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Only4KafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/only4_kafka/Only4KafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.only4_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Only4KafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
