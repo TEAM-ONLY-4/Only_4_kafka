e71b6bc589858ea83a233cd46fadb23f9a2ea874
HyeongseoShin <tlsgudtj123@gmail.com>
2026-01-17 22:39:35 +0900

    [FEAT]: SMS 청구서 Dto 생성 & BillRepository JPQL 구문 추가

--- src/main/java/com/example/only4_kafka/domain/bill/BillRepository.java
+++ src/main/java/com/example/only4_kafka/domain/bill/BillRepository.java
@@ -1,10 +1,39 @@
 package com.example.only4_kafka.domain.bill;
 
+import com.example.only4_kafka.domain.bill_send.SmsBillDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface BillRepository extends JpaRepository<Bill, Long> {
+    @Query("""
+        SELECT SmsBillDto(
+            m.name,
+            m.phoneNumber,
+            m.doNotDisturbStartTime,
+            m.doNotDisturbEndTime,
+            
+            b.paymentOwnerNameSnapshot,
+            b.paymentNameSnapshot,
+            b.paymentNumberSnapshot,
+            b.dueDate,
+
+            b.id,
+            b.billingYearMonth,
+            b.totalAmount,
+            b.totalDiscountAmount,
+            b.unpaidAmount,
+            b.totalBilledAmount,
+            b.vat,
+            CURRENT_DATE
+        )
+        FROM Bill b
+        JOIN b.member m
+        WHERE b.id = :billId
+    """)
+    Optional<SmsBillDto> findSmsBillDtoById(@Param("billId") Long billId);
 }
---
+++ src/main/java/com/example/only4_kafka/domain/bill_send/SmsBillDto.java
@@ -0,0 +1,32 @@
+package com.example.only4_kafka.domain.bill_send;
+
+import com.example.only4_kafka.domain.receipt.PaymentMethod;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public record SmsBillDto(
+        // 고객 정보
+        String name,
+        String phoneNumber,
+        LocalTime doNotDisturbStartTime,
+        LocalTime doNotDisturbEndTime,
+
+        // 납부 정보
+        String paymentOwnerName,
+        String paymentName,
+        String paymentNumber,
+        LocalDate dueDate, // 납기일
+
+        // 명세서 정보
+        Long billId,
+        LocalDate billingYearMonth, // 청구연월
+        BigDecimal totalAmount, // 총 사용 금액
+        BigDecimal totalDiscountAmount, // 총 할인 금액
+        BigDecimal unpaidAmount, // 미납금
+        BigDecimal totalBilledAmount, // 총 청구 금액 (실제 납부)
+        BigDecimal vat, // 부가가치세
+        LocalDate createdDate // 작성 일자
+) {
+}
