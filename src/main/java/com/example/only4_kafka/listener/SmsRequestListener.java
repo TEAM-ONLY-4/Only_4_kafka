02cfca34e9c9577eadd9b7102b10747eb1261051
HyeongseoShin <tlsgudtj123@gmail.com>
2026-01-17 16:52:03 +0900

    [FEAT]: Sms Dto, Listener, Service 생성

---
+++ src/main/java/com/example/only4_kafka/listener/SmsRequestListener.java
@@ -0,0 +1,33 @@
+package com.example.only4_kafka.listener;
+
+import com.example.only4_kafka.config.properties.KafkaTopicsProperties;
+import com.example.only4_kafka.event.SmsSendRequestEvent;
+import com.example.only4_kafka.service.SmsSendService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class SmsRequestListener {
+    private final KafkaTopicsProperties topics;
+    private final SmsSendService smsSendService;
+
+    public SmsRequestListener(KafkaTopicsProperties topics, SmsSendService smsSendService) {
+        this.topics = topics;
+        this.smsSendService = smsSendService;
+    }
+
+    @KafkaListener(
+            topics = "${app.kafka.topics.sms-request}",
+            groupId = "${app.kafka.topics.group-id}"
+
+            // 노션에 적힌 아래 방법은 에러가 떠서 바꿈
+            // A component required a bean named 'kafkaTopicsProperties' that could not be found.
+//            topics = "#{@kafkaTopicsProperties.smsRequest}",
+//            groupId = "#{@kafkaTopicsProperties.groupId}"
+    )
+    public void listen(SmsSendRequestEvent message) {
+        smsSendService.send(message);
+    }
+}
