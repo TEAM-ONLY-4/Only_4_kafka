8d1c396e4097916b3a5ac986ef8a3901d5e0bb6e
HyeongseoShin <tlsgudtj123@gmail.com>
2026-01-18 01:20:33 +0900

    [FEAT] SMS 1% 발송 실패 코드 추가

--- src/main/java/com/example/only4_kafka/infrastructure/sms/SmsClient.java
+++ src/main/java/com/example/only4_kafka/infrastructure/sms/SmsClient.java
@@ -1,20 +1,23 @@
 package com.example.only4_kafka.infrastructure.sms;
 
+import com.example.only4_kafka.domain.bill_notification.BillNotificationRepository;
 import com.example.only4_kafka.global.exception.BusinessException;
 import com.example.only4_kafka.global.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.ThreadLocalRandom;
 
+
 @Slf4j
+@RequiredArgsConstructor
 @Component
 public class SmsClient {
-    public void send(String phoneNumber, Long billId, String smsBillContent) {
-        // 이미 청구서가 발송되었는지 확인하기
-        // 문제점 : '아직 발송중'인 경우 체킹 가능? => 발송은 했으나 도착 여부는 모를 때
+    private final BillNotificationRepository billNotificationRepository;
 
-        // SMS 발송 시도
+    public void send(String phoneNumber, Long billId, String smsBillContent) {
+        // SMS 발송 시도 : 일단 1% 확률로 실패 처리
         int random = ThreadLocalRandom.current().nextInt(100);
 
         // 뽑은 숫자가 0이면 실패 (확률 1/100)
