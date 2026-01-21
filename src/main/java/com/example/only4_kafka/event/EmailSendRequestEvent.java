abf35097578d55bc14a5b257838df09dc03213ed
HyeongseoShin <tlsgudtj123@gmail.com>
2026-01-21 15:27:56 +0900

    [CHORE]: Email Sms Event에 @Builder 추가

--- src/main/java/com/example/only4_kafka/event/EmailSendRequestEvent.java
+++ src/main/java/com/example/only4_kafka/event/EmailSendRequestEvent.java
@@ -1,5 +1,8 @@
 package com.example.only4_kafka.event;
 
+import lombok.Builder;
+
+@Builder
 public record EmailSendRequestEvent(
         Long memberId,
         Long billId
--- src/main/java/com/example/only4_kafka/event/SmsSendRequestEvent.java
+++ src/main/java/com/example/only4_kafka/event/SmsSendRequestEvent.java
@@ -1,5 +1,8 @@
 package com.example.only4_kafka.event;
 
+import lombok.Builder;
+
+@Builder
 public record SmsSendRequestEvent(
         Long memberId,
         Long billId
